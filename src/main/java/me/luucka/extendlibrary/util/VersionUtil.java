78750da21037926cb6f689b224a05d7c36fd075a
LuuckA <luca.dalu@outlook.it>
2023-08-13 18:36:45 +0200

    Add VersionUtil utility class

---
+++ src/main/java/me/luucka/extendlibrary/util/VersionUtil.java
@@ -0,0 +1,158 @@
+package me.luucka.extendlibrary.util;
+
+import com.google.common.base.Objects;
+import org.bukkit.Bukkit;
+
+import java.util.logging.Level;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public final class VersionUtil {
+
+    public static final ServerVersion v1_13_2_R01 = ServerVersion.fromString("1.13.2-R0.1-SNAPSHOT");
+    public static final ServerVersion v1_14_4_R01 = ServerVersion.fromString("1.14.4-R0.1-SNAPSHOT");
+    public static final ServerVersion v1_15_2_R01 = ServerVersion.fromString("1.15.2-R0.1-SNAPSHOT");
+    public static final ServerVersion v1_16_5_R01 = ServerVersion.fromString("1.16.5-R0.1-SNAPSHOT");
+    public static final ServerVersion v1_17_1_R01 = ServerVersion.fromString("1.17.1-R0.1-SNAPSHOT");
+    public static final ServerVersion v1_18_2_R01 = ServerVersion.fromString("1.18.2-R0.1-SNAPSHOT");
+    public static final ServerVersion v1_19_4_R01 = ServerVersion.fromString("1.19.4-R0.1-SNAPSHOT");
+    public static final ServerVersion v1_20_1_R01 = ServerVersion.fromString("1.20.1-R0.1-SNAPSHOT");
+
+    private static ServerVersion serverVersion = null;
+
+    public static ServerVersion getServerVersion() {
+        if (serverVersion == null) {
+            serverVersion = ServerVersion.fromString(Bukkit.getServer().getBukkitVersion());
+        }
+        return serverVersion;
+    }
+
+    public static boolean isServerVersionSupported(ServerVersion minVersion) {
+        return getServerVersion().isHigherThanOrEqualTo(minVersion);
+    }
+
+    private VersionUtil() {
+    }
+
+    public static final class ServerVersion implements Comparable<ServerVersion> {
+
+        private static final Pattern VERSION_PATTERN = Pattern.compile("^(\\d+)\\.(\\d+)\\.?([0-9]*)?(?:-?R?([\\d.]+))?(?:-SNAPSHOT)?");
+
+        private final int major;
+        private final int minor;
+        private final int patch;
+        private final double revision;
+
+        private ServerVersion(final int major, final int minor, final int patch, final double revision) {
+            this.major = major;
+            this.minor = minor;
+            this.patch = patch;
+            this.revision = revision;
+        }
+
+        public static ServerVersion fromString(final String string) {
+            Matcher matcher = VERSION_PATTERN.matcher(string);
+            if (!matcher.matches()) {
+                Bukkit.getLogger().log(Level.SEVERE, string + " is not in valid version format. e.g. 1.20.1-R0.1");
+            }
+            return from(matcher.group(1), matcher.group(2), matcher.group(3), matcher.group(4));
+        }
+
+        private static ServerVersion from(final String major, final String minor, String patch, String revision) {
+            if (patch == null || patch.isEmpty()) patch = "0";
+            if (revision == null || revision.isEmpty()) revision = "0";
+            return new ServerVersion(
+                    Integer.parseInt(major),
+                    Integer.parseInt(minor),
+                    Integer.parseInt(patch),
+                    Double.parseDouble(revision)
+            );
+        }
+
+
+        public boolean isHigherThan(final ServerVersion o) {
+            return compareTo(o) > 0;
+        }
+
+        public boolean isHigherThanOrEqualTo(final ServerVersion o) {
+            return compareTo(o) >= 0;
+        }
+
+        public boolean isLowerThan(final ServerVersion o) {
+            return compareTo(o) < 0;
+        }
+
+        public boolean isLowerThanOrEqualTo(final ServerVersion o) {
+            return compareTo(o) <= 0;
+        }
+
+        public int getMajor() {
+            return major;
+        }
+
+        public int getMinor() {
+            return minor;
+        }
+
+        public int getPatch() {
+            return patch;
+        }
+
+        public double getRevision() {
+            return revision;
+        }
+
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            final ServerVersion that = (ServerVersion) o;
+            return major == that.major &&
+                    minor == that.minor &&
+                    patch == that.patch &&
+                    revision == that.revision;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(major, minor, patch, revision);
+        }
+
+        @Override
+        public String toString() {
+            final StringBuilder sb = new StringBuilder(major + "." + minor);
+            if (patch != 0) {
+                sb.append(".").append(patch);
+            }
+            return sb.append("-R").append(revision).toString();
+        }
+
+        @Override
+        public int compareTo(final ServerVersion o) {
+            if (major < o.major) {
+                return -1;
+            } else if (major > o.major) {
+                return 1;
+            } else { // equal major
+                if (minor < o.minor) {
+                    return -1;
+                } else if (minor > o.minor) {
+                    return 1;
+                } else { // equal minor
+                    if (patch < o.patch) {
+                        return -1;
+                    } else if (patch > o.patch) {
+                        return 1;
+                    } else { // equal patch
+                        return Double.compare(revision, o.revision);
+                    }
+                }
+            }
+        }
+    }
+
+}
